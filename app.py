b80fe3d901f8b3bc8d8cc5ce0240e43cf498e10c
Alif Budiman <82330418+alipbudiman@users.noreply.github.com>
2022-07-26 20:37:47 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=message))
     else:
         message = "Hallo apa kamu keren? kalo iya, cuma karen yang bisa jawab pertanyaan ini."
-        messgae += "\napa kepanjangan dari FIND ?"
+        message += "\napa kepanjangan dari FIND ?"
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=message))
             
                  
